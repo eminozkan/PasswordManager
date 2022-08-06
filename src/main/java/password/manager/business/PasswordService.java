113bb25d91edc0539c656467109c36590b23c0b8
Emin <info@eminozkan.net>
2022-08-06 15:24:32 +0300

    Updated password-service -created list-update-delete functions -for list func added hide-pass-inf func -changed AddPasswordResults to PasswordOperationResults -created getPassInfById func -set generate-password func to static -created isPasswordEmpty func for save-update-delete funcs

--- src/main/java/password/manager/business/PasswordService.java
+++ src/main/java/password/manager/business/PasswordService.java
@@ -3,10 +3,12 @@ package password.manager.business;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import password.manager.business.password.Generator;
 import password.manager.business.password.Password;
-import password.manager.persistence.AddPasswordResults;
+import password.manager.business.results.PasswordOperationResults;
 import password.manager.persistence.PasswordRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -22,13 +24,67 @@ public class PasswordService {
         return ObjectUtils.isEmpty(pass.getTitle());
     }
 
-    public AddPasswordResults savePassword(Password pass){
+    private Boolean isPasswordEmpty(Password pass){
+        return ObjectUtils.isEmpty(pass);
+
+    }
+
+    private List<Password> hidePasswordInf(List<Password> passwords){
+        for(Password pass : passwords){
+            int length = pass.getPassword().length();
+            for(int i = 0; i < length;i++){
+                StringBuffer sb = new StringBuffer();
+                sb.append("*");
+                pass.setPassword(sb.toString());
+            }
+        }
+        return passwords;
+    }
+    public PasswordOperationResults savePassword(Password pass){
         if(isPasswordTitleEmpty(pass)){
-            return AddPasswordResults.TITLE_IS_NULL;
+            return PasswordOperationResults.TITLE_IS_NULL;
+        }else if(!isPasswordEmpty(pass)){
+            return PasswordOperationResults.PASSWORD_EXISTS;
         }
         pass.setId(UUID.randomUUID().toString());
         repo.save(pass);
-        return AddPasswordResults.SUCCESS;
+        return PasswordOperationResults.SUCCESS;
     }
+
+
+
+    public List<Password> listPassword(){
+        List<Password> passwords =  hidePasswordInf(repo.list());
+        return passwords;
+    }
+
+    public PasswordOperationResults updatePassword(Password pass){
+        if(isPasswordEmpty(pass)){
+            return PasswordOperationResults.PASSWORD_NOT_EXISTS;
+        }
+        repo.save(pass);
+        return PasswordOperationResults.SUCCESS;
+    }
+
+    public PasswordOperationResults deletePassword(Password pass){
+        if(isPasswordEmpty(pass)){
+            return PasswordOperationResults.PASSWORD_NOT_EXISTS;
+        }
+        repo.deleteById(pass.getId());
+        return PasswordOperationResults.SUCCESS;
+    }
+
+    public String getPasswordInfById(String id){
+        Password pass = repo.findById(id);
+        return pass.getPassword();
+    }
+
+    public String generatePassword(Integer length){
+        return Generator.generatePassword(length);
+    }
+
+
+
+
 
 }
--- src/main/java/password/manager/business/password/Generator.java
+++ src/main/java/password/manager/business/password/Generator.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 
-    @Service
+@Service
 public class Generator {
-        private String CHARACTERS = "1234567890!@#$%&*()./AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        private static String CHARACTERS = "1234567890!@#$%&*()./AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
-        public String generatePassword(Integer Length){
+        public static String generatePassword(Integer Length){
             StringBuffer stringBuffer = new StringBuffer();
             SecureRandom rnd = new SecureRandom();
             for(int counter = 0; counter < Length; counter++){
