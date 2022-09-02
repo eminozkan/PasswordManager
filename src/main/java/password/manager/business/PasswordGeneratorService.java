9c34de30104a2432f08bffc53228f68cd2ceee08
Emin <info@eminozkan.net>
2022-09-02 15:29:41 +0300

    Refactored Controllers with new Services

--- src/main/java/password/manager/business/PasswordGeneratorService.java
+++ src/main/java/password/manager/business/PasswordGeneratorService.java
@@ -1,11 +1,10 @@
 package password.manager.business;
 
-import org.springframework.stereotype.Service;
 import password.manager.business.password.Password;
 import password.manager.business.password.PasswordGenerateOptions;
 import password.manager.business.results.PasswordOperationResults;
 
-@Service
+
 public interface PasswordGeneratorService {
 
     Password generatePasswordWithNoId(PasswordGenerateOptions options);
--- src/main/java/password/manager/presentation/PasswordGeneratorController.java
+++ src/main/java/password/manager/presentation/PasswordGeneratorController.java
@@ -1,44 +1,42 @@
-//package password.manager.presentation;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.util.ObjectUtils;
-//import org.springframework.web.bind.annotation.*;
-//import password.manager.business.password.GeneratedPassword;
-//import password.manager.business.password.Password;
-//import password.manager.business.results.PasswordOperationResults;
-//
-//@RestController
-//@RequestMapping("/api/passwords/generate")
-//public class PasswordGeneratorController {
-//    private final DefaultPasswordService passwordService;
-//
-//    public PasswordGeneratorController(DefaultPasswordService passwordService) {
-//        this.passwordService = passwordService;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<GeneratedPassword> generatePassword(@RequestBody GeneratedPassword password){
-//        if(ObjectUtils.isEmpty(password.getPasswordId())) {
-//            GeneratedPassword generatedPass = passwordService.generatePassword(password);
-//            return new ResponseEntity<>(generatedPass, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
-//    }
-//
-//    @PatchMapping()
-//    public ResponseEntity<Password> generatePasswordById(@RequestBody GeneratedPassword generatedPassword){
-//        if(ObjectUtils.isEmpty(generatedPassword.getPasswordId())){
-//            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
-//        }
-//        PasswordOperationResults result = passwordService.generatePassword(generatedPassword.getPasswordId(),generatedPassword);
-//        if(result == PasswordOperationResults.PASSWORD_NOT_EXISTS){
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        Password pass = new Password(passwordService.getPasswordById(generatedPassword.getPasswordId(),false));
-//        return new ResponseEntity<>(pass,HttpStatus.OK);
-//
-//    }
-//
-//
-//}
+package password.manager.presentation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import password.manager.business.PasswordGeneratorService;
+import password.manager.business.password.Password;
+import password.manager.business.password.PasswordGenerateOptions;
+import password.manager.business.results.PasswordOperationResults;
+
+@RestController
+@RequestMapping("/api/passwords/generate")
+public class PasswordGeneratorController {
+
+    private final PasswordGeneratorService generatorService;
+
+    public PasswordGeneratorController(PasswordGeneratorService service) {
+        this.generatorService = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Password> generatePassword(@RequestBody PasswordGenerateOptions options){
+        if(ObjectUtils.isEmpty(options.getPasswordId())) {
+            Password generatedPass = generatorService.generatePasswordWithNoId(options);
+            return new ResponseEntity<>(generatedPass, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @PatchMapping()
+    public ResponseEntity<String> generatePasswordById(@RequestBody PasswordGenerateOptions options){
+        if(ObjectUtils.isEmpty(options.getPasswordId())){
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        PasswordOperationResults result = generatorService.generatePasswordWithId(options);
+        if(result == PasswordOperationResults.PASSWORD_NOT_EXISTS){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("Success",HttpStatus.OK);
+    }
+}
